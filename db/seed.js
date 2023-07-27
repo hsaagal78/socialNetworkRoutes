f03f5ee2da70bfec7ee7e57bd29d5bfaafee8f51
hsaagal78 <hsagal78@gmail.com>
2023-07-27 16:24:30 -0400

    added a seed.js file and ready to try the connection

---
+++ db/seed.js
@@ -0,0 +1,64 @@
+const users = [
+    {
+      username: 'john_doe',
+      email: 'john.doe@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'jane_smith',
+      email: 'jane.smith@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'mike_jackson',
+      email: 'mike.jackson@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'sarah_adams',
+      email: 'sarah.adams@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'alex_robinson',
+      email: 'alex.robinson@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'emily_williams',
+      email: 'emily.williams@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'david_brown',
+      email: 'david.brown@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'lisa_turner',
+      email: 'lisa.turner@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'ryan_smith',
+      email: 'ryan.smith@example.com',
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: 'olivia_johnson',
+      email: 'olivia.johnson@example.com',
+      thoughts: [],
+      friends: [],
+    },
+  ];
+
+  module.exports = users;
