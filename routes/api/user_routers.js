462974a9c726d2bcb1ebf2426f3816719d31477b
hsaagal78 <hsagal78@gmail.com>
2023-07-28 08:33:34 -0400

    Router get to show user by Id is created and tested by insomnia

--- routes/api/user_routers.js
+++ routes/api/user_routers.js
@@ -2,10 +2,43 @@ const router = require("express").Router();
 const  User  = require("../../models/User");
 const { Thought } = require("../../models/Thought");
 
+
+//Show all the User
 router.get('/', async (req, res ) => {
     
     try {
         const users = await User.find().populate([
+        
+            {
+                path: "friends",
+                select: "_v",
+            },
+            {
+                path: "thoughts",
+                select: "_v",
+                populate: {
+                    path: "reactions",
+                    select: "-__v",
+                  },
+            },
+        ]);
+        res.json(users);
+
+    }catch(err) {
+        console.log('error');
+        res.status(401).send({
+            message: err.message,
+            
+        });
+    }
+
+});
+
+//Show user by Id. 
+router.get('/:id', async (req, res ) => {
+    
+    try {
+        const users = await User.findOne({_id: req.params.id}).populate([
         
             {
                 path: "friends",
